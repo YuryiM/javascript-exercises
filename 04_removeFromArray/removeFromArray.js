4f41c23aadc2cb006f0a9d9a3b3d86dcb6e55d3d
Yuryi Mironchyk <ymironchyk@gmail.com>
2022-04-05 12:09:06 -0700

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item1, item2, item3, item4, item5) {
+    return array.filter(item => { 
+        for(i = 1; i < arguments.length; i++){
+            if (item === arguments[i]){
+                break;
+            }
+            else if (i == arguments.length-1) {
+                return item;
+            }
+        }
+    });
 };
 
 // Do not edit below this line
